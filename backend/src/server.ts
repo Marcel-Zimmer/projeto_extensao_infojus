10929a0b38c9bc8254ae9d19ce14a3cac1327c32
Marcel Zimmer <Marcel-zimmer>
2025-06-17 20:20:48 -0300

    começo do backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Olá');
+});
+
+app.post('/createQuestion',(req: Request, res: Response) => {
+  res.send('Olá');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
